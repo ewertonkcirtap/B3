3627a0c0c25f610c5dab1e54afc199244564926c
Ewerton Kcirtap <ewertonkcirtap@Mac-mini-de-Ewerton.local>
2024-05-08 18:15:17 -0300

    files

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import requests
+
+def fundamentus () :
+
+    url = 'http://fundamentus.com.br/resultado.php'
+
+    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"}
+
+    r = requests.get(url,headers=header)
+
+    return pd.read_html(r.text,  decimal=',', thousands='.')[0]
+
+df = fundamentus()
+
+
+
